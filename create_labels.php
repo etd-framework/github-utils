64869657c517d98888a084577edee811c4fd52e2
Jean-Baptiste Alleaume <jbanety@gmail.com>
2016-12-12 10:50:29 +0100

    Création des labels sur les repos

---
+++ create_labels.php
@@ -0,0 +1,79 @@
+<?php
+
+// Composer's autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Method to detect the requested URI from server environment variables.
+ *
+ * @return  string  The requested URI
+ *
+ * @since   1.0
+ */
+function detectRequestUri() {
+
+    $input = new \Joomla\Input\Input();
+
+    $serverSSLVar    = $input->server->getString('HTTPS', '');
+    $isSslConnection = (!empty($serverSSLVar) && strtolower($serverSSLVar) != 'off');
+
+    $scheme = $isSslConnection ? 'https://' : 'http://';
+
+    $phpSelf    = $input->server->getString('PHP_SELF', '');
+    $requestUri = $input->server->getString('REQUEST_URI', '');
+
+    // If PHP_SELF and REQUEST_URI are both populated then we will assume "Apache Mode".
+    if (!empty($phpSelf) && !empty($requestUri)) {
+        $uri = $scheme . $input->server->getString('HTTP_HOST') . $requestUri;
+    } else {
+        // IIS uses the SCRIPT_NAME variable instead of a REQUEST_URI variable... thanks, MS
+        $uri       = $scheme . $input->server->getString('HTTP_HOST') . $input->server->getString('SCRIPT_NAME');
+        $queryHost = $input->server->getString('QUERY_STRING', '');
+
+        // If the QUERY_STRING variable exists append it to the URI string.
+        if (!empty($queryHost)) {
+            $uri .= '?' . $queryHost;
+        }
+    }
+
+    return trim($uri);
+}
+
+// SSL !!!!!!
+
+$uri = new \Joomla\Uri\Uri(detectRequestUri());
+
+if (!$uri->isSSL()) {
+    $uri->setScheme("https");
+    header('Location: ' . $uri->toString());
+    die;
+}
+
+?>
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Création des labels</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<form action="/do_labels.php" method="post">
+
+    <fieldset>
+        <legend>Labels</legend>
+        <input type="password" name="token" value="" placeholder="token">
+        <input type="text" name="owner" value="jbanety" placeholder="owner">
+        <input type="text" name="repo" value="" placeholder="repo">
+        <button type="submit">boum!</button>
+    </fieldset>
+
+</form>
+
+</body>
+</html>
---
+++ do_labels.php
@@ -0,0 +1,115 @@
+<?php
+
+// Composer's autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+$input = new \Joomla\Input\Input();
+$token = $input->get('token', '', 'string');
+$owner = $input->get('owner', '', 'string');
+$repo  = $input->get('repo', '', 'string');
+
+if (empty($token) || empty($owner) || empty($repo)) {
+    die('invalid params');
+}
+
+// GitHub
+
+$options = new \Joomla\Registry\Registry;
+$options->set('gh.token', $token);
+
+$github = new \Joomla\Github\Github($options);
+
+// Labels
+$labels = [
+
+    // Plateforme
+    "framework" => "BFD4F2",
+    "app"       => "BFD4F2",
+    "site"      => "BFD4F2",
+
+    // Problèmes
+    "bug"      => "EE3F46",
+    "sécurité" => "EE3F46",
+    "prod"     => "F45D43",
+
+    // Ennuyeux
+    "corvée"  => "FEF2C0",
+    "contenu" => "FFF2C1",
+
+    // Expérience
+    "design" => "FFC274",
+    "ux"     => "FFC274",
+
+    // Environnement
+    "staging" => "FAD8C7",
+    "tests"   => "FAD8C7",
+
+    // Feedback
+    "discussion"  => "CC317C",
+    "question"    => "CC317C",
+    "besoin aide" => "CC317C",
+
+    // Améliorations
+    "amélioration" => "5EBEFF",
+    "optimisation" => "5EBEFF",
+
+    // Ajouts
+    "fonction" => "91CA55",
+
+    // En attente
+    "en cours"    => "FBCA04",
+    "a surveillé" => "FBCA04",
+
+    // Inactive
+    "invalide"   => "D2DAE1",
+    "wontfix"    => "D2DAE1",
+    "doublon"    => "D2DAE1",
+    "en attente" => "D2DAE1",
+
+    // Fixes rapides
+    "boum"   => "FFFF00",
+    "45mins" => "FFFF00"
+    
+];
+
+foreach ($labels AS $label => $color) {
+
+    echo "Traitement du label &quot;" . htmlspecialchars($label) . "&quot;... ";
+
+    try {
+
+        // On tente de récupérer le label.
+        $github->issues->labels->get($owner, $repo, $label);
+
+        // Le label existe, on le met à jour.
+        $github->issues->labels->update($owner, $repo, $label, $label, $color);
+
+        echo "mis à jour !";
+
+
+    } catch (\Exception $e) {
+
+        // Le label n'existe pas, on le crée
+        if ($e->getCode() == 404) {
+
+            try {
+
+                // Le label existe, on le met à jour.
+                $github->issues->labels->create($owner, $repo, $label, $color);
+
+                echo "créé !";
+
+            } catch (\Exception $e) {
+                die($e->getCode() . " : " . $e->getMessage());
+            }
+
+        } else {
+            die($e->getCode() . " : " . $e->getMessage());
+        }
+
+    }
+
+    echo "<br>";
+
+}
+
